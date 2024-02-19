402e03b1200c9e0f2a01b9b87e8d48ed435a3477
berilakin <160046946+berilakin@users.noreply.github.com>
2024-02-19 20:09:22 +0300

    Update ders1.py
    .

--- ders1.py
+++ ders1.py
@@ -1,1 +1,17 @@
-sözlük
+#sözlük
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "SHEESH": "onaylamamak",
+            "ROFL": "bir şakaya karşılık cevap",
+            "CREEPY": "korkunç",
+            "AGGRO": "agresifleşmek" 
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    new_word = input("eklemek istediğiniz kelimeyi yazın (hepsini büyük harflerle yazın!):")
+    meme_dict.append(new_word)
